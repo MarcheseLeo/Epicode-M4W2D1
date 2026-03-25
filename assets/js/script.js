960da5595b38d04c762a2be09be965eaa20b0ee9
MarcheseLeo <leonardo.lol.ldp@gmail.com>
2026-03-25 12:51:23 +0100

    adding snackbar

--- assets/js/script.js
+++ assets/js/script.js
@@ -120,8 +120,17 @@ search.addEventListener('click', async (e) => {
                 })
 
             })
+    } else {
+        showSnackbar('Il campo input non puó essere vuoto')
     }
 })
+
+const showSnackbar = (message) => {
+    var x = document.getElementById("snackbar");
+    x.className = "show";
+    x.innerHTML = message
+    setTimeout(function () { x.className = x.className.replace("show", ""); }, 3000);
+}
 
 
 
